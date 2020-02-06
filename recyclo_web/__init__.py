b3b0592dd9c2519a3cbab0969902c2471181ec2f
Calix <wingtat.calix@gmail.com>
2020-02-07 02:20:55 +0800

    modified folder structures, contact from user model is set to null=True, added Signup backend (template just to check) including field validation

---
+++ recyclo_web/__init__.py
@@ -0,0 +1,22 @@
+from app import app
+from flask import render_template
+from recyclo_web.blueprints.users.views import users_blueprint
+from flask_assets import Environment, Bundle
+from .util.assets import bundles
+
+assets = Environment(app)
+assets.register(bundles)
+
+app.register_blueprint(users_blueprint, url_prefix="/users")
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('500.html'), 500
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.route("/")
+def home():
+    return render_template('home.html')
