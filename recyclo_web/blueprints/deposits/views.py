36a7e234cd688b85c52af6509adfae4718309f4e
Stephen <stephen.sjbrown@gmail.com>
2020-02-07 16:00:56 +0800

    added endpoints to create and show deposits

--- recyclo_web/blueprints/deposits/views.py
+++ recyclo_web/blueprints/deposits/views.py
@@ -1,9 +1,9 @@
 from flask import Blueprint, render_template, redirect, request, url_for, flash, jsonify
 from models.user import User, Deposit
 from models.centre import Centre
-from flask_login import current_user
+from flask_login import current_user, login_manager
 
-desposits_blueprint = Blueprint('deposits',
+deposits_blueprint = Blueprint('deposits',
                             __name__,
                             template_folder='templates')
 
@@ -18,22 +18,34 @@ def create():
     points = weight*100
 
 
-    new_deposit = Deposit.create(user=user, centre=centre, weight=weight, points=points)
-    new_deposit.execute()
+    new_deposit = Deposit(user=user, centre=centre, weight=weight, points=points)
+    new_deposit.save()
 
     user.update(points=User.points + points) 
     
-    return jsonify(centre=centre.name,
-                   weight=new_deposit.weight,
-                   points=points)
+    return jsonify({'centre':centre.name,
+                   'weight':new_deposit.weight,
+                   'points':points})
 
-@deposits_blueprint.route('/show')
-def show():
-    user = User.get_by_id(current_user.id)
+@deposits_blueprint.route('/show/<id>')
+def show(id):
+    user = User.get_by_id(id)
 
-    return jsonify(
-                date=deposit.created_at,
-                centre=deposit.centre,
-                weight=deposit.weight,
-                points=deposit.points
-    )
+    # user = User.get_by_id(1)
+    deposits = user.deposits
+
+
+    return jsonify([{
+                'date':deposit.created_at,
+                'centre':deposit.centre.id,
+                'weight':deposit.weight,
+                'points':deposit.points} for deposit in deposits
+    ]), 200
+
+    # else:
+    #     return jsonify([{
+    #             'date':deposit.created_at,
+    #             'centre':deposit.centre.id,
+    #             'weight':deposit.weight,
+    #             'points':deposit.points} for deposit in deposits
+    #     ]), 418
