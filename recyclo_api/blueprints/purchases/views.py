4f5c182648373166f648bccc0f40d47324789a02
Calix <wingtat.calix@gmail.com>
2020-02-11 15:40:53 +0800

    modified seed file

--- recyclo_api/blueprints/purchases/views.py
+++ recyclo_api/blueprints/purchases/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, redirect, request, url_for, flash, jsonify
-from models.user import User, Coupon
+from models.user import User, Purchase
+from models.store import Coupon
 from flask_login import current_user
 
 purchases_blueprint = Blueprint('purchases',
@@ -7,12 +8,21 @@ purchases_blueprint = Blueprint('purchases',
                             template_folder='templates')
 
 
-@purchases_blueprint.route('/<coupon_id>', methods=['GET'])
-def create(coupon_id):
-    coupon = Coupon.get_or_none(Coupon.id == coupon_id)
-    user = User.get_or_none(current_user.id == User.id)
-    if coupon and user:
-        new_purchase = Purchase(user_id=user.id, coupon_id=coupon.id)
-        new_purchase.save()
+@purchases_blueprint.route('/', methods=['POST'])
+def create():
+    buy = request.get_json()
+
+    coupon = Coupon.get_or_none(Coupon.id == buy['coupon_id'])
+    if coupon:
+        user = User.get_or_none(current_user.id == buy['user_id'])
+        if coupon and user:
+            new_purchase = Purchase(user_id=user.id, coupon_id=coupon.id)
+            new_purchase.save()
+            return jsonify({'message' : 'purchase success'}) 
+        else:
+            return jsonify({'message' : 'user not found'})
+    else:
+        return jsonify({'message' : 'coupon not found'})
+
 
 
--- seed.py
+++ seed.py
@@ -1,5 +1,5 @@
 from models.base_model import db
-from models.user import User
+from models.user import User, Purchase
 from models.store import Store, Coupon
 
 user_data = [
@@ -13,7 +13,7 @@ user_data = [
 ]
 
 store_data = [
-    {'name':'mcdonald','password':'Password123','email':'mcdonald@gmail.com','category':'food'},
+    {'name':'mcdonalds','password':'Password123','email':'mcdonalds@gmail.com','category':'food'},
     {'name':'kfc','password':'Password123','email':'kfc@gmail.com','category':'food'},
     {'name':'tealive','password':'Password123','email':'tealive@gmail.com','category':'food'},
     {'name':'uniqlo','password':'Password123','email':'uniqlo@gmail.com','category':'clothing'},
@@ -26,15 +26,24 @@ store_data = [
 ]
 
 coupon_data = [
-    {'store_id':'1','name':'McRabbit promo','value':25,'description':'delicious rabbit','cost':5000,'expiration':'1/1/2021'},
-    {'store_id':'1','name':'McDaddy buy 2 free 1','value':33,'description':'Happy father\'s day','cost':5000,'expiration':'1/1/2021'},
-    {'store_id':'1','name':'McDooDoo buy 1 free 1','value':50,'description':'doodoo nice','cost':10000,'expiration':'1/1/2021'},
-    {'store_id':'2','name':'Run chicken RUN!','value':20,'description':'catch yo chicken','cost':8000,'expiration':'1/1/2021'},
-    {'store_id':'2','name':'Cheeky nuggets','value':30,'description':'nuggets4life','cost':5000,'expiration':'1/1/2021'},
-    {'store_id':'3','name':'Boba for life','value':15,'description':'Boba for life','cost':5000,'expiration':'1/1/2021'},
-    {'store_id':'12','name':'comfy clothes','value':25,'description':'very comfy clothes','cost':5000,'expiration':'1/1/2021'},
-    {'store_id':'12','name':'comfy pants','value':35,'description':'very comfy pants','cost':10000,'expiration':'1/1/2021'},
-    {'store_id':'13','name':'not so comfy pants','value':75,'description':'very not comfy pants','cost':5000,'expiration':'1/1/2021'}
+    {'store_id':1,'name':'McRabbit promo','value':25,'description':'delicious rabbit','cost':5000,'expiration':'1/1/2021'},
+    {'store_id':1,'name':'McDaddy buy 2 free 1','value':33,'description':'Happy father\'s day','cost':5000,'expiration':'1/1/2021'},
+    {'store_id':1,'name':'McDooDoo buy 1 free 1','value':50,'description':'doodoo nice','cost':10000,'expiration':'1/1/2021'},
+    {'store_id':2,'name':'Run chicken RUN!','value':20,'description':'catch yo chicken','cost':8000,'expiration':'1/1/2021'},
+    {'store_id':2,'name':'Cheeky nuggets','value':30,'description':'nuggets4life','cost':5000,'expiration':'1/1/2021'},
+    {'store_id':3,'name':'Boba for life','value':15,'description':'Boba for life','cost':5000,'expiration':'1/1/2021'},
+    {'store_id':4,'name':'comfy clothes','value':25,'description':'very comfy clothes','cost':5000,'expiration':'1/1/2021'},
+    {'store_id':4,'name':'comfy pants','value':35,'description':'very comfy pants','cost':10000,'expiration':'1/1/2021'},
+    {'store_id':5,'name':'not so comfy pants','value':75,'description':'very not comfy pants','cost':5000,'expiration':'1/1/2021'}
+]
+
+purchase_data = [
+    {"user_id":1,"coupon_id":1,"status":"active"},
+    {"user_id":1,"coupon_id":1,"status":"redeemed"},
+    {"user_id":2,"coupon_id":1,"status":"expired"},
+    {"user_id":4,"coupon_id":2,"status":"active"},
+    {"user_id":7,"coupon_id":2,"status":"active"},
+    {"user_id":5,"coupon_id":2,"status":"expired"}
 ]
 
 def seed_users():
@@ -55,5 +64,11 @@ def seed_users():
             Coupon.create(**row)
     except:
         print('coupon problem')
+
+    try:
+        for row in db.batch_commit(purchase_data, 100):
+            Purchase.create(**row)
+    except:
+        print('purchase problem')
 
     print('seed complete')
