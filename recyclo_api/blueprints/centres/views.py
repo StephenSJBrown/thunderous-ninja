a332956eb3daa2df9eabfb0423edb171760e7fd8
Calix Kong <57282645+LB6996@users.noreply.github.com>
2020-02-16 00:01:57 +0800

    Update views.py

--- recyclo_api/blueprints/centres/views.py
+++ recyclo_api/blueprints/centres/views.py
@@ -48,45 +48,4 @@ def index():
 
 
 
-    #get all centres
-
-    # centres = Centre.select()
-    # return jsonify([{
-    #     'id' : centre.id,
-    #     'name' : centre.name,
-    #     'place_id' : centre.place_id
-    # } for centre in centres
-    # ])
-
-
-
-    # ipc = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=IPC%20Recycling%20&%20Buy-Back%20Centre&inputtype=textquery&fields=place_id,photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBn7jYGlUVP-bN2rXnr2gQDy2ZxzKOcgvk')
-    # kayu = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Tzu%20Chi%20Kayu%20Ara%20Recycle%20Center&inputtype=textquery&fields=place_id,photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBn7jYGlUVP-bN2rXnr2gQDy2ZxzKOcgvk')
-    # tzuchi = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=Tzu%20Chi%20Recycling%20Point&inputtype=textquery&fields=place_id,photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBn7jYGlUVP-bN2rXnr2gQDy2ZxzKOcgvk')
-    # nextaca = requests.get('https://maps.googleapis.com/maps/api/place/findplacefromtext/json?input=NEXT%20Academy&inputtype=textquery&fields=place_id,photos,formatted_address,name,rating,opening_hours,geometry&key=AIzaSyBn7jYGlUVP-bN2rXnr2gQDy2ZxzKOcgvk')
-    # coor = ipc.json()['candidates'][0]['geometry']['location']
-    # print(coor['lat'],coor['lng'])
-    # obj = {
-    #     'lat' :coor['lat'],
-    #     'lng' :coor['lng']
-    # }
-    # coor = [ipc.json(), kayu.json(), tzuchi.json(), nextaca.json()]
-    # return jsonify(coor)
-
-    # next_place_id = ChIJh819ADlJzDERuOXOhQOZCB8
-
-    # next_academy_place_id = 
-
-#hardcode place_id to centre table
-#user will parse in their location (lat & lng) when click, server shall return closest centre with name, place_id, & distance.
-#user will select desired centre and app will run googleMap with inputs all parsed.
-
-
-# user input location name
-# returns input's place_id
-# returns distance between user's place_id and centre's place_id
-
-
-# user chooses a centre
-# runs google MAP api, with origin and destination place id attached.
-
+ 
